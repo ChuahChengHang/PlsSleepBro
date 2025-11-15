a906f80413f2555decd29165e350f8f537278bef
ChuahChengHang <chuah_cheng_hang@s2024.ssts.edu.sg>
2025-11-15 21:46:58 +0800

    Initial Commit

---
+++ PlsSleepBro/PlsSleepBroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlsSleepBroApp.swift
+//  PlsSleepBro
+//
+//  Created by Chuah Cheng Hang on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PlsSleepBroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
